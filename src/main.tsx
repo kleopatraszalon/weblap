723204bc76d9fd9ed63896c21d1acf6be9d393bd
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-08 12:35:14 +0200

    Load modern homepage and header styling

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
 import "./styles/public-pages.css";
 import "./styles/commerce-pages.css";
+import "./styles/home-modern.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
