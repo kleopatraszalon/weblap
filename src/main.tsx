e387aff7f4f385fa64ed2ceab9ca909f8c11e758
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-08 11:03:20 +0200

    Load unified public page styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
+import "./styles/public-pages.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
