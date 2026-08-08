1706e28f3da0d082551d0609456a45dd8143c1ef
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-08 11:10:33 +0200

    Load commerce page brand styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
 import "./styles/public-pages.css";
+import "./styles/commerce-pages.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
