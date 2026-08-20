de55c58869148f98a118a0dc504b1cae6825d80b
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-20 20:54:26 +0200

    feat(booking-v4): install booking funnel tracker

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { installMarketingAttribution } from "./marketingAttribution";
+import { installBookingFunnelTracking } from "./bookingFunnel";
 import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
 import "./styles/public-pages.css";
@@ -10,6 +11,7 @@ import "./styles/home-modern.css";
 import "./styles/booking-modern.css";
 
 installMarketingAttribution();
+installBookingFunnelTracking();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
