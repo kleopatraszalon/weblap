dd76b0e2251d90e1766e69580b8cf5b45a0d42de
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-21 16:32:06 +0200

    feat: expose complete public navigation hierarchy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,13 +25,15 @@ export function Header() {
 
         <div className={"kleo-modern-header__nav-wrap" + (open ? " is-open" : "")}>
           <nav className="kleo-modern-nav" aria-label="Fő navigáció">
+            <NavLink to="/services" className={navLinkClass} onClick={close}>Szolgáltatások</NavLink>
+            <NavLink to="/prices" className={navLinkClass} onClick={close}>Áraink</NavLink>
             <NavLink to="/salons" className={navLinkClass} onClick={close}>Szalonjaink</NavLink>
             <NavLink to="/loyalty" className={navLinkClass} onClick={close}>Hűségprogram</NavLink>
-            <NavLink to="/prices" className={navLinkClass} onClick={close}>Áraink</NavLink>
             <NavLink to="/webshop" className={navLinkClass} onClick={close}>{t("menu.webshop")}</NavLink>
-            <NavLink to="/franchise" className={navLinkClass} onClick={close}>Franchise</NavLink>
-            <NavLink to="/career" className={navLinkClass} onClick={close}>Karrier</NavLink>
             <NavLink to="/education" className={navLinkClass} onClick={close}>Oktatás</NavLink>
+            <NavLink to="/career" className={navLinkClass} onClick={close}>Karrier</NavLink>
+            <NavLink to="/about" className={navLinkClass} onClick={close}>Rólunk</NavLink>
+            <NavLink to="/franchise" className={navLinkClass} onClick={close}>Franchise</NavLink>
           </nav>
 
           <div className="kleo-modern-header__tools">
