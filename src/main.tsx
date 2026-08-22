320007beee593c1206e468f01626142956c96fb5
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-22 18:24:25 +0200

    fix(signage): use v15 grid-child info cards

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { installMarketingAttribution } from "./marketingAttribution";
 import { installSignageMotionV8 } from "./signageMotionV8";
 import { installSignageOfferSpotlightV11 } from "./signageOfferSpotlightV11";
-import { installSignageInfoCardsV14 } from "./signageInfoCardsV14";
+import { installSignageInfoCardsV15 } from "./signageInfoCardsV15";
 import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
 import "./styles/public-pages.css";
@@ -14,12 +14,12 @@ import "./styles/booking-modern.css";
 import "./styles/signage-responsive-v5.css";
 import "./styles/signage-motion-v8.css";
 import "./styles/signage-offer-spotlight-v11.css";
-import "./styles/signage-info-cards-v14.css";
+import "./styles/signage-info-cards-v15.css";
 
 installMarketingAttribution();
 installSignageMotionV8();
 installSignageOfferSpotlightV11();
-installSignageInfoCardsV14();
+installSignageInfoCardsV15();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
