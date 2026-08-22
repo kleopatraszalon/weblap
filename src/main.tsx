523ed6a02d3cc1e1b1eeb7ce7fbb0ac7e2514081
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-22 19:06:45 +0200

    fix(signage): mount React v17 overlay

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import SignageOverlayV17 from "./components/SignageOverlayV17";
 import { installMarketingAttribution } from "./marketingAttribution";
 import { installSignageMotionV8 } from "./signageMotionV8";
 import { installSignageOfferSpotlightV11 } from "./signageOfferSpotlightV11";
-import { installSignageInfoCardsV16 } from "./signageInfoCardsV16";
 import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
 import "./styles/public-pages.css";
@@ -14,15 +14,15 @@ import "./styles/booking-modern.css";
 import "./styles/signage-responsive-v5.css";
 import "./styles/signage-motion-v8.css";
 import "./styles/signage-offer-spotlight-v11.css";
-import "./styles/signage-info-cards-v16.css";
+import "./styles/signage-overlay-v17.css";
 
 installMarketingAttribution();
 installSignageMotionV8();
 installSignageOfferSpotlightV11();
-installSignageInfoCardsV16();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <SignageOverlayV17 />
   </React.StrictMode>
 );
