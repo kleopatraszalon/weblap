369db9e696db3898aa1faf1d43472ccd82842f62
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-22 20:42:36 +0200

    feat(signage): enable v22 readable cards

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { installMarketingAttribution } from "./marketingAttribution";
 import { installSignageMotionV8 } from "./signageMotionV8";
 import { installSignageOfferSpotlightV11 } from "./signageOfferSpotlightV11";
-import { installSignageNativeV21 } from "./signageNativeV21";
+import { installSignageNativeV22 } from "./signageNativeV22";
 import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
 import "./styles/public-pages.css";
@@ -14,12 +14,12 @@ import "./styles/booking-modern.css";
 import "./styles/signage-responsive-v5.css";
 import "./styles/signage-motion-v8.css";
 import "./styles/signage-offer-spotlight-v11.css";
-import "./styles/signage-native-v21.css";
+import "./styles/signage-native-v22.css";
 
 installMarketingAttribution();
 installSignageMotionV8();
 installSignageOfferSpotlightV11();
-installSignageNativeV21();
+installSignageNativeV22();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
