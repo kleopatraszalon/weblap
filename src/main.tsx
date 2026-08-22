c6fce57f0fdc7ac7f462d0c6da5dbf1ccc5ef28d
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-22 18:09:14 +0200

    fix(signage): switch from legacy roaming dock to v14 lanes

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { installMarketingAttribution } from "./marketingAttribution";
 import { installSignageMotionV8 } from "./signageMotionV8";
-import { installSignageMotionV10 } from "./signageMotionV10";
 import { installSignageOfferSpotlightV11 } from "./signageOfferSpotlightV11";
-import { installSignageInfoCardsV13 } from "./signageInfoCardsV13";
+import { installSignageInfoCardsV14 } from "./signageInfoCardsV14";
 import "./styles/kleo-theme.css";
 import "./styles/website-cms.css";
 import "./styles/public-pages.css";
@@ -14,15 +13,13 @@ import "./styles/home-modern.css";
 import "./styles/booking-modern.css";
 import "./styles/signage-responsive-v5.css";
 import "./styles/signage-motion-v8.css";
-import "./styles/signage-motion-v10.css";
 import "./styles/signage-offer-spotlight-v11.css";
-import "./styles/signage-info-cards-v13.css";
+import "./styles/signage-info-cards-v14.css";
 
 installMarketingAttribution();
 installSignageMotionV8();
-installSignageMotionV10();
 installSignageOfferSpotlightV11();
-installSignageInfoCardsV13();
+installSignageInfoCardsV14();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
