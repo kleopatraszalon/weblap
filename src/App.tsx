ee51a050cabcaeb0eba7422eb5f063608f5a347d
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-23 14:22:54 +0200

    feat(booking): activate revised booking flow

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ import { WebshopProductDetailPage } from "./pages/WebshopProductDetailPage";
 import { CartPage } from "./pages/CartPage";
 import { CheckoutPage } from "./pages/CheckoutPage";
 import { BookingPage as VoiceBookingPage } from "./pages/BookingPage";
-import { BookingPageV5 } from "./pages/BookingPageV5";
+import { BookingPageV6 } from "./pages/BookingPageV6";
 import { API_BASE } from "./apiClient";
 import type { CartItem } from "./utils/cart";
 import { LanguageProvider } from "./i18n";
@@ -103,9 +103,9 @@ function AppShell() {
       <Route path="/kiosk/*" element={<KioskPage />} />
       <Route path="/" element={franchiseHost ? <FranchiseV1Page /> : <HomePageEnhanced />} />
 
-      <Route path="/booking" element={<BookingPageV5 />} />
-      <Route path="/idopontfoglalas" element={<BookingPageV5 />} />
-      <Route path="/foglalas" element={<BookingPageV5 />} />
+      <Route path="/booking" element={<BookingPageV6 />} />
+      <Route path="/idopontfoglalas" element={<BookingPageV6 />} />
+      <Route path="/foglalas" element={<BookingPageV6 />} />
       <Route path="/hangos-idopontfoglalas" element={<VoiceBookingPage />} />
 
       <Route path="/salons" element={<SalonsPage />} />
