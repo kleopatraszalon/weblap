51f64f76dec0705b564f152a4b03fd7117fb3cd9
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-23 09:12:08 +0200

    feat: modern VIR-style mobile navigation
    * feat(web): redesign mobile header navigation
    
    * style(web): add premium mobile drawer navigation
    
    * style(web): load mobile header overrides

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import "./styles/public-pages.css";
 import "./styles/commerce-pages.css";
 import "./styles/home-modern.css";
 import "./styles/booking-modern.css";
+import "./styles/mobile-header.css";
 import "./styles/signage-responsive-v5.css";
 import "./styles/signage-motion-v8.css";
 import "./styles/signage-offer-spotlight-v11.css";
