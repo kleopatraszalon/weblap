a3e96c5f9d684d6d1cc47ed3433c73109b665057
kleopatraszalon <birtalan.zoltan1975@gmail.com>
2026-08-29 19:53:56 +0200

    style(kiosk): load theme service artwork styles

--- src/pages/KioskPage.tsx
+++ src/pages/KioskPage.tsx
@@ -9,6 +9,7 @@ import "./kiosk/kiosk-kids.css";
 import "./kiosk/kiosk-face-body-mapping.css";
 import "./kiosk/kiosk-mapping-launcher-hotfix.css";
 import "./kiosk/kiosk-mapping-recommendations.css";
+import "./kiosk/kiosk-theme-service-art.css";
 import { KioskShell } from "./kiosk/KioskShell";
 import { KioskLanding } from "./kiosk/KioskLanding";
 import { KioskCategory } from "./kiosk/KioskCategory";
